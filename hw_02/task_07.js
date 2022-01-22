1ee04f7e8d7e988bf5f4d8f060aba6fa3cf088e8
Your Name <you@example.com>
2022-01-22 13:37:10 +0200

    hw-02-task-07

--- hw_02/task_07.js
+++ hw_02/task_07.js
@@ -26,17 +26,28 @@
 
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-const isLoginValid = function (login) {
-  // код
-};
+function isLoginValid(login) {
+  let loginLength = login.length;
+  return loginLength >= 4 && loginLength <= 16;
+}
 
-const isLoginUnique = function (allLogins, login) {
-  // код
-};
+function isLoginUnique(allLogins, login) {
+  return !allLogins.includes(login);
+}
 
-const addLogin = function (allLogins, login) {
-  // код
-};
+function addLogin(allLogins, login) {
+  if (!isLoginValid(login)) {
+    console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
+  } else if (!isLoginUnique(allLogins, login)) {
+    console.log('Такой логин уже используется');
+    return 'Такой логин уже используется';
+  } else {
+    logins.push(login);
+    console.log('Логин успешно добавлен');
+    return 'Логин успешно добавлен';
+  }
+}
 
 // Вызовы функции для проверки
 addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
